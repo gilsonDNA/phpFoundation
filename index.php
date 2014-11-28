178dbaf979407bad28daa3268abdc119c236326a
Gilson Anselmo de Araujo <gilson@MacBook-Pro-de-Gilson.local>
2014-11-27 22:09:22 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>PHP Foundation</title>
+</head>
+<body>
+
+   <?php require_once("menu.php"); ?>
+    <div>
+        <h1>
+            HOME
+        </h1>
+
+        <?php require_once($_GET("arquivo")); ?>
+    </div>
+
+</body>
+</html>
