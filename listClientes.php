e0f9f77647e8fb24d618863714662b4e567aca33
Gilson <gilson@idnadevendas.com.br>
2015-01-21 15:40:28 -0200

    Criando Ordenação

--- listClientes.php
+++ listClientes.php
@@ -11,10 +11,11 @@ include __DIR__ . "/admin/cliente/clienteDAO.php";
         <h1> Clientes Cadastrados </h1>
         <hr />
 
+        <?php if(!isset( $_POST['submit']) ): ?>
         <!-- MONTANDO A TABELA -->
         <table class="table table-striped">
             <tr>
-                <td><b>Id</b></td>
+                <td><b><input class="btn btn-primary btn-small" type="submit" name="submit" value="Id (ordem decrescente) "/></b></td>
                 <td><b>Nome</b></td>
                 <td><b>Cpf</b></td>
                 <td><b>Visualizar</b></td>
@@ -44,6 +45,48 @@ include __DIR__ . "/admin/cliente/clienteDAO.php";
         ?>
 
         </table>
+
+        <?php else: ?>
+
+        <!-- MONTANDO A TABELA -->
+        <table class="table table-striped">
+            <tr>
+                <td><b><input class="btn btn-primary btn-small" type="submit" name="submit" value="Id (ordem crescente) "/></b></td>
+                <td><b>Nome</b></td>
+                <td><b>Cpf</b></td>
+                <td><b>Visualizar</b></td>
+            </tr>
+
+            <?php
+
+            /**
+             * Retorna o array de objetos, depois percorre montando a lista de Clientes
+             */
+
+            $listaClientes = getListClientes();
+            arsort($listaClientes);
+
+            foreach($listaClientes as $cliente)
+            {
+
+                echo "<tr>";
+                echo "<td>" . $cliente->getId() . "</td>";
+                echo "<td>" . $cliente->getNome() . "</td>";
+                echo "<td>" . $cliente->getCpf() . "</td>";
+                echo "<td>";
+                echo "<a href='detalheCliente.php?key=" . $cliente->getId() . "' title='clique para acessar'>Ver Detalhes</a></td>";
+                echo "</td></tr>";
+
+
+            }
+
+            ?>
+
+        </table>
+
+
+        <?php endif; ?>
+
 
         <hr />
 
